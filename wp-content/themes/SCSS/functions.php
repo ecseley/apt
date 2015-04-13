35ca29a8811f296caebf4f2147165f1828fff47a
Endré Cseley <ecseley.fya@gmail.com>
2015-04-13 07:53:18 -0400

    playing around with design: Mac Pro --> Mac Book Pro

--- wp-content/themes/SCSS/functions.php
+++ wp-content/themes/SCSS/functions.php
@@ -7,3 +7,23 @@ function my_assets() {
 }
 
 add_action( 'wp_enqueue_scripts', 'my_assets' );
+
+add_action( 'wp_enqueue_scripts', 'prefix_enqueue_awesome' );
+/**
+ * Register and load font awesome CSS files using a CDN.
+ *
+ * @link   http://www.bootstrapcdn.com/#fontawesome
+ * @author FAT Media
+ */
+function prefix_enqueue_awesome() {
+	wp_enqueue_style( 'prefix-font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css', array(), '4.0.3' );
+}
+
+/**
+ * Enqueue Animate.css
+ */
+// function animateCSS() {
+// 	wp_enqueue_style( 'animate', get_stylesheet_uri() . '/assets/css/animate.css/animate.min.css' );
+// }
+
+// add_action( 'wp_enqueue_scripts', 'animateCSS' );
